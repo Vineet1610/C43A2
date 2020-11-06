8267c7b9eab7cb320117fd3f5bf802c1c2b82d05
Raj Patel <devraj5698@yahoo.ca>
2020-11-06 15:44:26 -0500

    Finished Queries 2,4,6,8 in sql file and methods 1,3,5,7 in java file (#1)
    * Finished Queries 2,4,6,8 in sql file and methods 1,3,5,7 in java file
    
    * Fixed ps.close() issue and formatted queries differently

--- Assignment2.java
+++ Assignment2.java
@@ -10,26 +10,68 @@ public class Assignment2 {
   
     // Prepared Statement
     PreparedStatement ps;
+    // Second Prepared Statment
+    PreparedStatement ps2;
   
     // Resultset for the query
     ResultSet rs;
+    // Second Resultset
+    ResultSet rs2;
   
     //CONSTRUCTOR
     Assignment2(){
+      try {
+        Class.forName("org.postgresql.Driver");
+      } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+      }
     }
   
     //Using the input parameters, establish a connection to be used for this session. Returns true if connection is sucessful
     public boolean connectDB(String URL, String username, String password){
-        return false;
+      try {
+        if (connection != null && !connection.isClosed()) {
+            if (!disconnectDB()) {
+                return false;
+            }
+        }
+
+        connection = DriverManager.getConnection(URL, username, password);
+        return true;
+      } catch (Exception e) {
+          return false;
+      }
     }
   
-    //Closes the connection. Returns true if closure was sucessful
+    // Closes the connection. Returns true if closure was successful
     public boolean disconnectDB(){
         return false;    
     }
     
     public boolean insertPlayer(int pid, String pname, int globalRank, int cid) {
-        return false;
+      try {
+        ps = connection.prepareStatement("INSERT INTO A2.player VALUES (?, ?, ?, ?);");
+
+        ps.setInt(1, pid);
+        ps.setString(2, pname);
+        ps.setInt(3, globalRank);
+        ps.setInt(4, cid);
+
+        int inserted = ps.executeUpdate();
+        try {
+            ps.close();
+        } catch (Exception e) {
+            return false;
+        }
+        return inserted == 1;
+      } catch (Exception e) {
+          try {
+              ps.close();
+          } catch (Exception e) {
+              return false;
+          }
+          return false;
+      }
     }
   
     public int getChampions(int pid) {
@@ -37,7 +79,49 @@ public class Assignment2 {
     }
    
     public String getCourtInfo(int courtid){
-        return "";
+      try {
+        ps = connection.prepareStatement("SELECT * FROM A2.court WHERE courtid=?;");
+
+        ps.setInt(1, courtid);
+        rs = ps.executeQuery();
+
+        if (rs.next()) {
+            int tid = rs.getInt("tid");
+            int capacity = rs.getInt("capacity");
+            String courtName = rs.getString("courtname");
+
+            ps2 = connection.prepareStatement("SELECT * FROM A2.tournament WHERE tid=?;");
+            ps2.setInt(1, tid);
+            rs2 = ps2.executeQuery();
+
+            if (rs2.next()) {
+                String tname = rs2.getString("tname");
+                return String.format("%d:%s:%d:%s", courtid, courtName, capacity, tname);
+            } else {
+                return "";
+            }
+        } else {
+            try {
+                ps.close();
+                ps2.close();
+                rs.close();
+                rs2.close();
+            } catch (Exception e) {
+                return "";
+            }
+            return "";
+        }
+      } catch (Exception e) {
+          try {
+              ps.close();
+              ps2.close();
+              rs.close();
+              rs2.close();
+          } catch (Exception e) {
+              return "";
+          }
+          return "";
+      }
     }
 
     public boolean chgRecord(int pid, int year, int wins, int losses){
@@ -45,7 +129,30 @@ public class Assignment2 {
     }
 
     public boolean deleteMatcBetween(int p1id, int p2id){
-        return false;        
+      try {
+        ps = connection.prepareStatement("DELETE FROM A2.event WHERE (winid=? AND lossid=?) OR (winid=? AND lossid=?);");
+
+        ps.setInt(1, p1id);
+        ps.setInt(2, p2id);
+        ps.setInt(3, p2id);
+        ps.setInt(4, p1id);
+
+        int count = ps.executeUpdate();
+        try {
+            ps.close();
+        } catch (Exception e) {
+            return false;
+        }
+
+        return count > 0;
+      } catch (Exception e) {
+          try {
+              ps.close();
+          } catch (Exception e) {
+              return false;
+          }
+          return false;
+      }
     }
   
     public String listPlayerRanking(){
@@ -53,7 +160,28 @@ public class Assignment2 {
     }
   
     public int findTriCircle(){
-        return 0;
+        try {
+            ps = connection.prepareStatement("SELECT COUNT(*) FROM (SELECT DISTINCT e1.winid, e2.winid, e3.winid "
+                    + "FROM A2.event e1, A2.event e2, A2.event e3 WHERE e1.winid < e2.winid AND e2.winid < e3.winid "
+                    + "AND e1.winid = e3.lossid AND e3.winid = e2.lossid AND e2.winid = e1.lossid) scope;");
+            rs = ps.executeQuery();
+
+            try {
+                ps.close();
+                rs.close();
+            } catch (Exception e) {
+                return 0;
+            }
+            return rs.next() ? rs.getInt(1) : 0;
+        } catch (Exception e) {
+            try {
+                ps.close();
+                rs.close();
+            } catch (Exception e) {
+                return 0;
+            }
+            return 0;
+        }
     }
     
     public boolean updateDB(){
--- a2.sql
+++ a2.sql
@@ -9,28 +9,125 @@ INSERT INTO query1
 
 --Query 2
 INSERT INTO query2
+(select tname, sum(capacity)
+from tournament join court on tournament.tid = court.tid
+group by tournament.tid
+having sum(capacity) = (select max(seats) from
+    (select tournament.tid, sum(capacity) as seats
+    from tournament join court on tournament.tid = court.tid
+    group by tournament.tid) tourneySeats)
+order by tname asc);
 
 --Query 3
 INSERT INTO query3
 
 --Query 4
 INSERT INTO query4
+(select distinct player.pid, pname 
+from champion join player on champion.pid = player.pid
+group by player.pid, pname
+having count (distinct tid) in (select count(distinct tid) from tournament)
+order by pname asc);
 
 --Query 5
 INSERT INTO query5
 
+--Views (Query 6)
+CREATE VIEW wins1 AS
+(select player.pid as pid, pname, wins
+from player join record on player.pid = record.pid
+where year = 2011);
+
+CREATE VIEW wins2 AS
+(select player.pid as pid, pname, wins
+from player join record on player.pid = record.pid
+where year = 2012);
+
+CREATE VIEW wins3 AS
+(select player.pid as pid, pname, wins
+from player join record on player.pid = record.pid
+where year = 2013);
+
+CREATE VIEW wins4 AS
+(select player.pid as pid, pname, wins
+from player join record on player.pid = record.pid
+where year = 2014);
+
 --Query 6
 INSERT INTO query6
+(select wins1.pid, wins1.pname
+from wins1
+join wins2 on wins1.pid = wins2.pid
+join wins3 on wins1.pid = wins3.pid
+join wins4 on wins1.pid = wins4.pid
+where (wins4 > wins3) and (wins3 > wins2) and (wins2 > wins1)
+order by pname asc);
+
+--Drop Views (Query 6)
+DROP VIEW wins1;
+DROP VIEW wins2;
+DROP VIEW wins3;
+DROP VIEW wins4;
 
 --Query 7
 INSERT INTO query7
 
 --Query 8
 INSERT INTO query8
+(select p1name, pname as p2name, cname
+from(
+	select pid, pname as p1name, cid as p1Country, opponentid 
+	from(
+		select winid as playerid, lossid as opponentid
+		from event
+		UNION
+		select lossid, winid
+		from event) matchups
+	join player on pid = playerid) p1Info
+join player on player.pid = opponentid
+join country on country.cid = player.cid
+where p1Country = player.cid
+order by cname asc, p1name desc);
 
 --Query 9
 INSERT INTO query9
 
+--Views (Query 10)
+CREATE VIEW WinLossTable2014 AS
+(select winner as pname, 
+	wins.courtname as court, 
+	wins.year as year, wins, losses
+from(select pname as winner, courtname, year, count(*) as wins
+	from event join player on winid = pid
+	join court on court.courtid = event.courtid
+	where year = 2014 group by pname, courtname, year) wins
+left join(select pname as loser, courtname, year, count(*) as losses
+	from event join player on lossid = pid
+	join court on court.courtid = event.courtid
+	where year = 2014 group by pname, courtname, year) losses
+on losses.loser = wins.winner and losses.courtname = wins.courtname);
+
+CREATE VIEW Played200MinutesAvg AS
+(select pname, avg(duration)
+from(select pname, duration
+	from event join player on lossid = pid
+	UNION ALL 
+    select pname, duration
+	from event join player on winid = pid) timeStats
+group by pname
+having avg(duration) > 200);
+
 --Query 10
 INSERT INTO query10
+(select distinct pname
+from WinLossTable2014
+where pname not in 
+	(select pname from WinLossTable2014 where losses >= wins)
+and pname in 
+	(select pname from Played200MinutesAvg)
+order by pname desc);
+
+--Drop views (Query 10)
+DROP VIEW WinLossTable2014;
+DROP VIEW Played200MinutesAvg;
 
