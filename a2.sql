a178e46294068ca8ff976c430488d8f85fe405ab
Raj Patel <devraj5698@yahoo.ca>
2020-11-06 23:24:42 -0500

    FML

--- a2.sql
+++ a2.sql
@@ -6,59 +6,20 @@ SET search_path TO A2;
 
 --Query 1
 INSERT INTO query1
-(
-select pname, cname, tname
-from tournament
-join country on country.cid = tournament.cid
-join champion on tournament.tid = champion.tid
-join player on champion.pid = player.pid
-where tournament.cid = player.cid
-order by pname asc
-);
 
 --Query 2
 INSERT INTO query2
-(select tname, sum(capacity) as totalCapacity
+(select tname, sum(capacity) 
 from tournament join court on tournament.tid = court.tid
 group by tournament.tid
 having sum(capacity) = (select max(seats) from
     (select tournament.tid, sum(capacity) as seats
     from tournament join court on tournament.tid = court.tid
-    group by tournament.tid) seats)
+    group by tournament.tid) tourneySeats)
 order by tname asc);
 
--- View for query 3
-CREATE VIEW allmatches AS
-(
-select player1 as player1id, pid as player2id, pname as player2name, globalrank as player2rank
-from (
-	(select winid as player1, lossid as player2
-	from event) 
-	UNION
-	(select lossid, winid
-	from event)
-) allmatches 
-left join player on pid = player2
-);
 --Query 3
 INSERT INTO query3
-(
-select p1id, p1name, pid as p2id, pname as p2name
-from (
-	select player1id as p1id, pname as p1name, highestGlobalRank
-	from (
-		select player1id, max(player2rank) as highestGlobalRank
-		from allmatches
-		group by player1id
-	) opponentPlayer
-	join player on player1id = pid
-) info 
-join player on globalrank = highestGlobalRank
-order by p1name
-);
-
--- Drop view allmatches for query 3
-DROP VIEW allmatches;
 
 --Query 4
 INSERT INTO query4
@@ -70,14 +31,6 @@ order by pname asc);
 
 --Query 5
 INSERT INTO query5
-(
-select player.pid, pname, (sum(wins)/cast(count(distinct year) as real)) as avgwins
-from player join record on player.pid = record.pid 
-where year = 2011 OR year = 2012 OR year = 2013 OR year = 2014
-group by player.pid, pname
-order by (sum(wins)/count(distinct year)) desc
-limit 10
-);
 
 --Views (Query 6)
 CREATE VIEW wins1 AS
@@ -118,13 +71,6 @@ DROP VIEW wins4;
 
 --Query 7
 INSERT INTO query7
-(
-select pname, year 
-from player join champion on player.pid = champion.pid
-group by pname, year
-having count(year) > 1
-order by pname desc, year desc
-);
 
 --Query 8
 INSERT INTO query8
@@ -145,60 +91,23 @@ order by cname asc, p1name desc);
 
 --Query 9
 INSERT INTO query9
-(
-select cname, count(*) as champions
-from champion
-join player on player.pid = champion.pid
-join country on country.cid = player.cid
-group by cname
-having count(*) = (select max(champs) 
-					from (
-					select count(*) as champs
-					from champion
-					join player on player.pid = champion.pid
-					join country on country.cid = player.cid
-					group by cname
-					) champ
-				  )
-order by cname desc
-);
 
 --Views (Query 10)
-CREATE VIEW winsandloss2014 AS
-(select winner as pname, 
-	wins.courtname as court, 
-	wins.year as year, wins, losses
-from(select pname as winner, courtname, year, count(*) as wins
-	from event join player on winid = pid
-	join court on court.courtid = event.courtid
-	where year = 2014 group by pname, courtname, year) wins
-left join(select pname as loser, courtname, year, count(*) as losses
-	from event join player on lossid = pid
-	join court on court.courtid = event.courtid
-	where year = 2014 group by pname, courtname, year) losses
-on losses.loser = wins.winner and losses.courtname = wins.courtname);
-
-CREATE VIEW minimum200minutes AS
-(select pname, avg(duration)
-from(select pname, duration
-	from event join player on lossid = pid
-	UNION ALL 
-    select pname, duration
-	from event join player on winid = pid) timeStats
-group by pname
-having avg(duration) > 200);
+CREATE VIEW greaterWinsThanLosses AS 
+(select * from 
+    (select * from record where year = 2014) 
+ where wins > losses);
+ 
+CREATE VIEW playerSubset as select pid from greaterWinsThanLosses;
+CREATE VIEW playerDurations as select pid, duration from playerSubset cross join event where pid = winid or pid = lossid;
+CREATE VIEW averageDuration as select pid, avg(duration) as avgDuration from playerDurations group by pid;
+CREATE VIEW greaterThan200Duration as select pid from averageDuration where avgDuration > 200;
+CREATE VIEW qualifiedPIDs as select pid from ((select pid from playerSubset) intersect (select pid from greaterThan200Duration));
+CREATE VIEW qualifiedPlayerNames as select pname from qualifiedPIDs natural join player;
 
 --Query 10
-INSERT INTO query10
-(select distinct pname
-from winsandloss2014
-where pname not in 
-	(select pname from winsandloss2014 where losses >= wins)
-and pname in 
-	(select pname from minimum200minutes)
-order by pname desc);
+INSERT INTO query10(select pname FROM qualifiedPlayerNames order by pname desc);
 
---Drop views (Query 10)
-DROP VIEW winsandloss2014;
-DROP VIEW minimum200minutes;
+--Drop Views
+DROP VIEW playerMatchesIn2014 CASCADE;
 
